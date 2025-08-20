1a457cc942e8d555aaec7eb6762eeecccb08b80c
sandip_1402 <sd5147083@gmail.com>
2025-08-21 00:52:52 +0530

    updated navbar

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,13 @@
     }, false)
   })
 })()
+
+const navItems = document.querySelectorAll('.nav-item');
+
+navItems.forEach(item => {
+  item.addEventListener('click', () => {
+    navItems.forEach(el => el.classList.remove('active'));
+    item.classList.add('active');
+  });
+});
+
