eca1955853763736a12a3e042caac3b8ddf4c9c9
sandip_1402 <sd5147083@gmail.com>
2026-04-05 19:59:31 +0530

    Integrating with auth0

--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
-import { Login } from "../components/Login";
-import { Signup } from "../components/Signup";
+import { useAuth0 } from "@auth0/auth0-react";
+export default function LoginButton() {
+  const { loginWithRedirect } = useAuth0();
 
-export const Test = () => {
-    return (
-        <div>
-            <Signup />
-        </div>
-    )
+  return (
+    <button onClick={() => loginWithRedirect()}>
+      Log In
+    </button>
+  );
 }
